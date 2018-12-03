04bdaa70c457ed4d7017a7393c65104ed0640e03
Felix Becker <git@felixbecker.name>
2018-12-04 00:05:59 +0100

    Improve performance of day 3 solution
    use array instead of sets..

--- src/main/scala/name/felixbecker/aoc/solutions/year2018/Day3.scala
+++ src/main/scala/name/felixbecker/aoc/solutions/year2018/Day3.scala
@@ -4,28 +4,46 @@ import scala.io.Source
 
 object Day3 extends App {
 
-  // First
-  case class Field(x: Int, y: Int)
-  case class Claim(id: Int, xoffs: Int, yoffs: Int, xsize: Int, ysize: Int){
-    def isDefinedAt(f: Field): Boolean = f.x > xoffs & f.x <= xoffs+xsize  && f.y > yoffs && f.y <= yoffs + ysize
-    def getCoveredFields: Set[Field] = (xoffs+1 to xoffs+xsize).flatMap { x => (yoffs+1 to yoffs + ysize).map { y => Field(x,y)}}.toSet
-  }
+  val minExecutionTime = (1 to 100).map { _ =>
+    val start = System.currentTimeMillis()
+    // First
+    case class Field(x: Int, y: Int)
+    case class Claim(id: Int, xoffs: Int, yoffs: Int, xsize: Int, ysize: Int) {
+      def isDefinedAt(f: Field): Boolean = f.x > xoffs & f.x <= xoffs + xsize && f.y > yoffs && f.y <= yoffs + ysize
 
-  val pattern = """^#([0-9]+)\s+@\s+([0-9]+),([0-9]+):\s+([0-9]+)x([0-9]+)$""".r
-  val claims = Source.fromResource("2018/day3/input.txt").getLines().map { case pattern(id, xoffs, yoffs, xsize, ysize) =>
-    Claim(id.toInt, xoffs.toInt, yoffs.toInt, xsize.toInt, ysize.toInt)
-  }.toList
+      def getCoveredFields: Set[Field] = (xoffs until xoffs + xsize).flatMap { x => (yoffs until yoffs + ysize).map { y => Field(x, y) } }.toSet
+    }
 
-  val allFields = collection.mutable.Map((1 to 1000).flatMap { x => (1 to 1000).map { y => (Field(x,y), 0)}}.toMap.toSeq: _*)
+    // the game doesn't start at 0, so we simply take 1000 fields (+1), and YES, that is waste of memory, but it makes
+    // the code more readable
+    val entireField = Array.fill(1009, 1000)(0)
+    val pattern = """^#([0-9]+)\s+@\s+([0-9]+),([0-9]+):\s+([0-9]+)x([0-9]+)$""".r
+    var moreThanOneFields = 0
+    val claims = Source.fromResource("2018/day3/input.txt").getLines().map { case pattern(id, xoffs, yoffs, xsize, ysize) =>
+      val c = Claim(id.toInt, xoffs.toInt, yoffs.toInt, xsize.toInt, ysize.toInt)
 
-  claims.foreach { c => c.getCoveredFields.foreach { f => allFields(f) = allFields(f) + 1}}
+      (c.xoffs until c.xoffs + c.xsize).foreach { x =>
+        (c.yoffs until c.yoffs + c.ysize).foreach { y =>
+          entireField(x)(y) += 1
+          if (entireField(x)(y) == 2) moreThanOneFields += 1
+        }
+      }
 
-  println(allFields.count { case (f, x) => x > 1})
+      c
+    }.toList
 
-  // Second
-  claims.find(c => c.getCoveredFields.count(f => allFields(f) > 1) == 0).foreach { c =>
-    println(s"Claim ID (which doesn't overlap): ${c.id}")
-  }
+    println(moreThanOneFields)
 
-  // TODO further improvement: Use arrays instead of maps to speed up..
+    // Second
+    claims.find(c => c.getCoveredFields.count(f => entireField(f.x)(f.y) > 1) == 0).foreach { c =>
+      println(s"Claim ID (which doesn't overlap): ${c.id}")
+    }
+
+    // Expected solutions: 97218 + 717
+    val end = System.currentTimeMillis()
+    println(s"Took ${end - start}ms")
+    end-start
+  }.min
+
+  println(minExecutionTime)
 }
