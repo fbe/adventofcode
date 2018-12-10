64647d396bf00bdb317d4ae06c23307a61064eed
Felix Becker <git@felixbecker.name>
2018-12-10 03:00:49 +0100

    Solution 2 of day 7 added

--- src/main/scala/name/felixbecker/aoc/solutions/year2018/Day7.scala
+++ src/main/scala/name/felixbecker/aoc/solutions/year2018/Day7.scala
@@ -9,11 +9,13 @@ case class BuildStep(id: BuildStepId, mustRunAfter: Set[BuildStepId])
 
 object Day7 extends App {
 
-  type BuildStepId = String
+  val fixedWorkTimeOffset = 60 // in sample 0
+
+  type BuildStepId = Char
 
   val buildSteps = Source.fromResource("2018/day7/input.txt").getLines().foldLeft[Map[BuildStepId, BuildStep]](Map.empty) { case (map, rule) =>
-    val dependsOn: BuildStepId = rule.substring(5,6)
-    val stepName: BuildStepId = rule.substring(36,37)
+    val dependsOn: BuildStepId = rule.charAt(5)
+    val stepName: BuildStepId = rule.charAt(36)
     val mapWithStep = map.get(stepName) match {
       case None => map + (stepName -> BuildStep(stepName, Set(dependsOn)))
       case Some(bs) => map + (stepName -> bs.copy(mustRunAfter = bs.mustRunAfter + dependsOn))
@@ -26,19 +28,54 @@ object Day7 extends App {
     }
   }
 
+  type SecondsTaken = Int
+  type WorkerId = Int
+  type Seconds = Int
+
+  case class WorkResult(stepOrder: List[BuildStepId], secondsTaken: SecondsTaken)
+  case class BusyWorker(buildStepId: BuildStepId, timeLeft: Seconds)
   @tailrec
-  def solveSteps(openSteps: Map[BuildStepId, BuildStep], orderedStepsDone: List[BuildStepId] = Nil): List[BuildStepId] = {
+  def solveSteps(openSteps: Map[BuildStepId, BuildStep],
+                 orderedStepsDone: List[BuildStepId] = Nil,
+                 parallelWorkers: Int = 1,
+                 busyWorkers: Set[BusyWorker] = Set.empty,
+                 secondsTaken: SecondsTaken = 0
+                ): WorkResult = {
+
     if(openSteps.isEmpty){
-      orderedStepsDone.reverse
+      WorkResult(orderedStepsDone.reverse, secondsTaken)
     } else {
-      val nextPossibleSteps = openSteps.values.filter(bs => bs.mustRunAfter.count(afterStepId => openSteps.contains(afterStepId)) == 0)
-      val nextStep = nextPossibleSteps.toList.minBy(_.id)
-      solveSteps(openSteps - nextStep.id, nextStep.id :: orderedStepsDone)
+
+      val possibleNextSteps = openSteps.values.filter { bs =>
+        bs.mustRunAfter.count(afterStepId => openSteps.contains(afterStepId)) == 0 &&
+          busyWorkers.count(bw => bw.buildStepId == bs.id) == 0
+      }.toList.sortBy(_.id)
+
+      if(busyWorkers.size == parallelWorkers || possibleNextSteps.isEmpty){
+
+        val nextFinish = busyWorkers.map(_.timeLeft).min
+        val nextFinishedBuildSteps = busyWorkers.filter(_.timeLeft == nextFinish).map(_.buildStepId)
+
+        solveSteps(
+          openSteps.filterKeys(k => !nextFinishedBuildSteps.contains(k)),
+          nextFinishedBuildSteps.toList.sorted ::: orderedStepsDone,
+          parallelWorkers,
+          busyWorkers.filterNot(_.timeLeft == nextFinish).map(remaining => remaining.copy(timeLeft = remaining.timeLeft - nextFinish)),
+          secondsTaken + nextFinish
+        )
+      } else {
+        val availableWorkers = parallelWorkers - busyWorkers.size
+        val nextSteps = possibleNextSteps.take(availableWorkers)
+        val newBusyWorkers = busyWorkers ++ nextSteps.map(ns => BusyWorker(ns.id, ns.id - 'A' + fixedWorkTimeOffset + 1)).toSet
+        solveSteps(openSteps, orderedStepsDone, parallelWorkers, newBusyWorkers, secondsTaken)
+      }
     }
   }
 
+  val result1 = solveSteps(buildSteps)
+  println(s"Solution 1: ${result1.stepOrder.mkString} - Time taken: ${result1.secondsTaken}")
 
-  println(s"Solution 1: ${solveSteps(buildSteps).mkString}")
-
+  val result2 = solveSteps(buildSteps, parallelWorkers = 5)
+  println(s"Solution 2: ${result2.stepOrder.mkString} - Time taken: ${result2.secondsTaken}")
 
 }
